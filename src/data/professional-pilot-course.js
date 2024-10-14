4b848c241e0a2b40369e76aad7629a066b883444
Raul Ospina <raminka13@hotmail.com>
2024-10-14 17:35:45 -0500

    New: pro pilot course page

---
+++ src/data/professional-pilot-course.js
@@ -0,0 +1,198 @@
+const professionalPilotCourse = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: "Professional Pilot Course | Sun City Aviation Academy",
+    pageDescription:
+      "Sun City Aviation Academy offers the Professional Pilot Course. An all in one comprehensive program which includes private pilot, instrument, commercial, and multi-engine training which sets you up to start a career in the aviation industry.",
+    pageKeywords:
+      "professional pilot course, airline pilot training, commercial pilot training, multi-engine training, sun city aviation academy",
+
+    //top header
+    header: {
+      stars: true,
+      imagePath: "/src/assets/john-mcarthur-PrdNTrIrG8w-unsplash.jpg",
+      imageAlt:
+        "sun city aviation academy trains professional pilots in south florida",
+      headerH1: `Professional</br><span class="text-accent-300">Pilot Course</span>`,
+      paragraph:
+        "Learn more about how to become a pilot with Sun City. We offer a variety of programs to help you achieve your aviation dreams and become a pilot.",
+      buttons: [
+        {
+          name: "Enroll Now",
+          link: "/marketing-system",
+          primary: false,
+        },
+        {
+          name: "Book a Discovery Flight",
+          link: "/schedule-call",
+          primary: false,
+        },
+      ],
+    },
+
+    pageSummary: {
+      title: "Zero to Hero",
+      paragraphs: [
+        "Sun City Aviation Academy offers the Professional Pilot Course. An all in one comprehensive program which includes private pilot, instrument, commercial, and multi-engine training which sets you up to start a career in the aviation industry.",
+        "We're experts in flight training and always put safety first. Choose Sun City Aviation Academy for your pilot training in the South Florida area.",
+      ],
+    },
+
+    zero2hero: {
+      heading: "Our Professional<br>Pilot Course",
+      upperHeading: "Breakdown",
+      description:
+        "Our Professional Pilot Course is structured to ensure progressive learning and skill acquisition through hands-on training, expert instruction, and access to modern aircraft. This program covers all necessary ground and flight training courses*, culminating in a Multi-Engine Rating.",
+    },
+
+    programs: {
+      upperHeading: "Explore",
+      heading: "Our Programs",
+      description:
+        "Your aviation dreams take center stage here. Whether pursuing a professional career, building flight hours, or seeking leisurely adventure, we have the perfect path for you.",
+      cards: [
+        {
+          title: "Professional Pilot Course",
+          imagePath: "/src/assets/sun-city-airline-pilot-ppc.webp",
+          imageAlt:
+            "sun city aviation professional pilots giving thumbs up in an airplane cockpit",
+          description:
+            "Comprehensive program covering private, instrument, commercial, and multi-engine flight training.",
+          link: "/professional-pilot-course",
+        },
+        {
+          title: "Private Pilot",
+          imagePath: "/src/assets/sun-city-private-pilot.webp",
+          imageAlt:
+            "sun city aviation private pilot training students in front of a plane",
+          description:
+            "Your passport to the skies! Learn to pilot your dreams with our Private Pilot Course.",
+          link: "/private-pilot-training",
+        },
+        {
+          title: "Group Ground School",
+          imagePath: "/src/assets/sun-city-group-ground-school-bg.webp",
+          imageAlt:
+            "sun city aviation group ground school students studying in a classroom",
+          description:
+            "Learn the fundamentals of aviation in a group setting and pass your pilot written exam.",
+          link: "/group-ground-school",
+        },
+        {
+          title: "Instrument Rating",
+          imagePath: "/src/assets/sun-city-instrument.webp",
+          imageAlt:
+            "sun city aviation certified flight instructor and student in a cockpit",
+          description:
+            "Navigate through clouds and the skies with precision through our Instrument Rating course.",
+          link: "/instrument-rating",
+        },
+        {
+          title: "Commercial Pilot",
+          imagePath: "/src/assets/sun-city-commercial.webp",
+          imageAlt:
+            "sun city aviation student and family displaying commercial pilot certificate",
+          description:
+            "Start your pilot career with our Commercial Pilot Course. Your path to the captain's seat is here.",
+          link: "/commercial-pilot-training",
+        },
+        {
+          title: "Airline Transport Pilot",
+          imagePath: "/src/assets/john-mcarthur-PrdNTrIrG8w-unsplash.jpg",
+          imageAlt:
+            "sun city aviation instruments panel of a commercial airline airplane cockpit",
+          description:
+            "For many this is their goal. The ATP certificate is a minimum requirement to join the airlines.",
+          link: "/airline-transport-pilot-atp",
+        },
+        {
+          title: "Multi-Engine Rating",
+          imagePath:
+            "/src/assets/sun-city-aviation-academy-multi-engine-aircraft.jpg",
+          imageAlt:
+            "sun city aviation student and flight instructor in front of a multi-engine airplane",
+          description:
+            "Master the art of multi engine flying and unlock new horizons in your aviation career.",
+          link: "/multi-engine-rating",
+        },
+        {
+          title: "Certified Flight Instructor",
+          imagePath: "/src/assets/sun-city-cfi-bg3.webp",
+          imageAlt:
+            "sun city aviation student and flight instructor shaking hands",
+          description:
+            "Become a trusted guide in the world of aviation. Become a CFI and teach others to fly.",
+          link: "/certified-flight-instructor-cfi",
+        },
+        {
+          title: "Flight Review",
+          imagePath: "/src/assets/sun-city-bfr.webp",
+          imageAlt: "sun city aviation cessna airplane on a runway",
+          description:
+            "Recurrent training requirement for pilots to ensure they maintain proficiency and safety standards.",
+          link: "/flight-review-bfr",
+        },
+        {
+          title: "Instrument Proficiency Check",
+          imagePath: "/src/assets/chuttersnap-M2-_GRvWWg0-unsplash.jpg",
+          imageAlt: "sun city aviation clouds and sky",
+          description:
+            "Stay current with our Instrument Proficiency Check (IPC) to ensure your instrument flying skills are up to date.",
+          link: "/instrument-proficiency-check-ipc",
+        },
+        {
+          title: "Wingman Course",
+          imagePath: "/src/assets/sun-city-wingman.jpg",
+          imageAlt: "sun city aviation pilot's family in the flight simulator",
+          description:
+            "Equip your loved ones with essential aviation safety skills through our Wingman Course.",
+          link: "/wingman-family-member-training",
+        },
+        {
+          title: "Sea Plane Safari",
+          imagePath: "/src/assets/sun-city-seaplane.jpeg",
+          imageAlt: "sun city aviation seaplane floating on the water",
+          description:
+            "Seaplane flying opens up an extraordinary new world for pilots. Start SES training here.",
+          link: "/seaplane-pilot-training",
+        },
+      ],
+    },
+
+    quizCTA: {
+      reverse: true,
+      upperHeading: "Take Our Quiz",
+      heading: "Do You Have What It Takes To Be A Pilot?",
+      descriptions: [
+        "Are you fascinated by the world of aviation? Have you dreamed of flying and exploring the skies? Do you often look upward at passing aircraft, wondering if you yourself could be in the cockpit?",
+        "Begin your journey in aviation with our brief quiz. Our quiz is designed to look at your aviation related knowledge and aptitude, alongside your unique personality traits and interests, to provide insights into whether a future in aviation aligns with your aspirations.",
+      ],
+      imagePath: "/src/assets/sun-city-aviation-academy-be-pilot-square.jpg",
+      imageAlt:
+        "sun city aviation academy student flying a plane over the city of miami",
+      buttons: [
+        {
+          name: "Take the Quiz",
+          link: "/quiz",
+          primary: true,
+        },
+      ],
+    },
+
+    flyWithUsCTA: {
+      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      headerH1: `<span class="text-primary-600">Start Your Aviation Journey</br></span>With a Discovery Flight`,
+      paragraph:
+        "Doing a discovery flight is the best way to determine if being a pilot is right for you. Whether you're trying it out for fun or want to fly professionally, doing a discovery flight will allow you to see the Ft. Lauderdale and Hollywood from a new perspective.",
+      buttons: [
+        {
+          name: "Schedule Today",
+          link: "/discovery-flight",
+          primary: true,
+        },
+      ],
+    },
+  },
+};
+export default professionalPilotCourse;
