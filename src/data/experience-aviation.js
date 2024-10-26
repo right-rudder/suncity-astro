13e817283b59f35ffcbce6c6f1c995f2bacee942
Raul Ospina <raminka13@hotmail.com>
2024-10-25 20:18:14 -0500

    New: experience aviation page and form

---
+++ src/data/experience-aviation.js
@@ -0,0 +1,138 @@
+const experienceAviationInfo = {
+  data: {
+    //SEO Header Stuff
+    pageTitle:
+      "Experience Aviation | Youth Programs | Sun City Aviation Academy",
+    pageDescription:
+      "Experience aviation with Sun City Aviation Academy. Our youth programs are designed to introduce young people to the world of aviation and inspire them to pursue a career in the field.",
+    pageKeywords:
+      "experience aviation, youth programs, aviation programs, aviation training, pilot training, flight school, sun city aviation academy",
+
+    //top header
+    header: {
+      stars: true,
+      imagePath: "/src/assets/AeroCamp165.jpg",
+      imageAlt:
+        "sun city aviation academy young program student smiling in a cessna cockpit",
+      headerH1: `Experience</br><span class="text-accent-300">Aviation</span>`,
+      paragraph:
+        "At Sun City Aviation Academy, we offer a range of youth programs designed to introduce young people to the world of aviation and inspire them to pursue a career in the field.",
+      buttons: [
+        {
+          name: "Enroll Now",
+          link: "#experience-aviation-form",
+          primary: false,
+        },
+        {
+          name: "Book a Discovery Flight",
+          link: "/discovery-flight",
+          primary: false,
+        },
+      ],
+    },
+
+    pageSummary: {
+      title: `What Is Experience Aviation?`,
+      paragraphs: [
+        "The Experience Aviation High School Program at Sun City Aviation Academy offers high school students a unique opportunity to explore the amazing world of aviation and aerospace at North Perry Airport.",
+        "Our certified instructors and team guide program attendees through a week of exciting experiences, including the chance to sit in the front of a plane and go flying.",
+        "Discover the wonders of aviation during this unforgettable adventure.",
+      ],
+    },
+
+    expectExpAviation: {
+      imagePath: "/src/assets/AeroCamp156.jpg",
+      imageAlt: "Sun City Aviation Academy young ",
+      heading: "Aviation Program for High School Students",
+      upperHeading: "What to Expect",
+      description: `Inspiring a new generation of aviators. Experience the joy of flight first hand.`,
+      paragraphs: [
+        "The Experience Aviation High School Program offers an exciting opportunity for high school students to do a deep dive into the world of aviation. Over three action packed days, participants will immerse themselves in the fundamentals of flight, learning about aerodynamics, radio communications, weather patterns, and more.",
+        "Led by experienced instructors, students will engage in hands on activities such as preflight inspections, interactive games, and simulator sessions.",
+        "They'll also go on guided tours of the Air Traffic Control Tower and/or Fire Station. Experience and learn how to fly in the flight simulator and at the end of the course, fly an actual plane. With interactive activities designed to enhance understanding and foster teamwork, this program is the perfect starting point for aspiring aviators.",
+      ],
+      list: {
+        heading: "Upcoming Program Dates",
+        items: [
+          {
+            title: "Basic Aviation",
+            icon: "M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z",
+            descriptions: ["December 20 - 21 - 22"],
+          },
+          {
+            title: "Advanced Aviation",
+            icon: "M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z",
+            descriptions: ["December 20 - 21 - 22"],
+          },
+        ],
+      },
+      afterListParagraphs: [
+        "The Experience Aviation High School Program is designed to provide students with a comprehensive introduction to the world of aviation. Participants will learn about the principles of flight, aircraft systems, and the role of the pilot. They will also have the opportunity to fly in a real airplane and experience the thrill of flight firsthand.",
+      ],
+      finalButton: {
+        name: "Enroll Now",
+        link: "#experience-aviation-form",
+        primary: true,
+      },
+    },
+
+    form: {
+      upperHeading: "Enroll Now",
+      heading: "Experience Aviation High School Program",
+      description: `Fill out the form below to enroll in the Experience Aviation High School Program at Sun City Aviation Academy. Our team will be in touch with you shortly to confirm your enrollment and provide you with more information about the program.`,
+      checkboxText: `I agree to <a href="/terms-of-service" target="_blank" class="font-semibold hover:underline">terms</a> & <a href="/privacy-policy" target="_blank" class="font-semibold hover:underline">conditions</a> provided by the company. By providing my phone number, I agree to receive text messages from the business.`,
+    },
+
+    bookCTA: {
+      upperHeading: "Still looking for more information on pilot training?",
+      headerH1: `Get Your <span class="text-accent-300">FREE</span><br> Pilot Training Guide!`,
+      paragraph:
+        "We have a free booklet which explains everything you need to know for your pilot certificate.",
+      buttons: [
+        {
+          name: "Download Now",
+          link: "/book-download",
+          primary: true,
+        },
+      ],
+      imagePath: "/src/assets/sun-city-book-mock-up.webp",
+      imageAlt: "Right Rudder Marketing team on the runway",
+    },
+
+    quizCTA: {
+      reverse: true,
+      upperHeading: "Take Our Quiz",
+      heading: "Do You Have What It Takes To Be A Pilot?",
+      descriptions: [
+        "Are you fascinated by the world of aviation? Have you dreamed of flying and exploring the skies? Do you often look upward at passing aircraft, wondering if you yourself could be in the cockpit?",
+        "Begin your journey in aviation with our brief quiz. Our quiz is designed to look at your aviation related knowledge and aptitude, alongside your unique personality traits and interests, to provide insights into whether a future in aviation aligns with your aspirations.",
+      ],
+      imagePath: "/src/assets/sun-city-aviation-academy-be-pilot-square.jpg",
+      imageAlt:
+        "sun city aviation academy student flying a plane over the city of miami",
+      buttons: [
+        {
+          name: "Take the Quiz",
+          link: "/quiz",
+          primary: true,
+        },
+      ],
+    },
+
+    flyWithUsCTA: {
+      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      headerH1: `<span class="text-primary-600">Start Your Aviation Journey</br></span>With a Discovery Flight`,
+      paragraph:
+        "Doing a discovery flight is the best way to determine if being a pilot is right for you. Whether you're trying it out for fun or want to fly professionally, doing a discovery flight will allow you to see the Ft. Lauderdale and Hollywood from a new perspective.",
+      buttons: [
+        {
+          name: "Schedule Today",
+          link: "/discovery-flight",
+          primary: true,
+        },
+      ],
+    },
+  },
+};
+export default experienceAviationInfo;
