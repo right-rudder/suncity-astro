1bc76a6d2900f87059a533d941c348af18332184
Raul Ospina <raminka13@hotmail.com>
2024-10-26 08:11:51 -0500

    New: aviation 101 page and form

---
+++ src/data/youth-aviation-classes.js
@@ -0,0 +1,122 @@
+const aviation101Info = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: "Aviation 101 | Youth Programs | Sun City Aviation Academy",
+    pageDescription:
+      "In our Aviation 101 - Ground Training Program, students aged 15 to 18 are invited to embark on a 13-week journey. Every Saturday morning from 10 am to 12 pm, participants will delve into an immersive course, culminating in test preparation and qualification for the Private Pilot Written Exam.",
+    pageKeywords:
+      "aviation 101, youth programs, sun city aviation academy, aviation 101 program, aviation 101 course, aviation 101 training, aviation 101 classes, aviation 101 for high school students, aviation 101 for teens, aviation 101 for kids, aviation 101 for youth, aviation 101 for teenagers, aviation 101 for children, aviation 101 for young people, aviation 101 for students, aviation 101 for high schoolers, aviation 101 for teenagers, aviation 101 for kids, aviation 101 for youth, aviation 101 for children, aviation 101 for young people, aviation 101 for students, aviation 101 for high schoolers, aviation 101 for teenagers, aviation 101 for kids, aviation 101 for youth, aviation 101 for children, aviation 101 for young people, aviation 101 for students, aviation 101 for high schoolers, aviation 101 for teenagers, aviation 101 for kids, aviation 101 for youth, aviation 101 for children, aviation 101 for young people, aviation 101 for students, aviation 101 for high schoolers, aviation 101 for teenagers, aviation 101 for kids, aviation 101 for youth, aviation 101 for children, aviation 101 for young people, aviation 101 for students, aviation 101 for high schoolers, aviation 101 for teenagers, aviation 101 for kids, aviation 101 for youth, aviation 101 for children, aviation 101 for young people, aviation 101 for students, aviation 101 for high schoolers, aviation 101 for teenagers, aviation 101 for kids, aviation 101 for youth, aviation 101 for children, aviation 101 for young people, aviation 101 for students, aviation 101 for high schoolers, aviation 101 for teenagers, aviation 101 for kids, aviation 101 for youth, aviation 101 for children, aviation 101 for young people, aviation 101 for students, aviation 101 for high schoolers, aviation 101 for teenagers, aviation 101 for kids, aviation 101 for youth, aviation 101 for children, aviation 101 for young people, aviation 101 for students, aviation 101 for high schoolers, aviation 101 for teenagers, aviation 101 for kids, aviation 101 for youth, aviation 101 for children, aviation 101 for young people, aviation 101 for students, aviation 101 for high schoolers, aviation",
+
+    //top header
+    header: {
+      stars: true,
+      imagePath: "/src/assets/AeroCamp11.jpg",
+      imageAlt:
+        "sun city aviation academy youth program students in the classroom",
+      headerH1: `Aviation</br><span class="text-accent-300">101</span>`,
+      paragraph:
+        "High School Aviation Program where the classroom is the sky. The High School Aviation Program is a unique opportunity designed to introduce younger minds to the marvels of flight.",
+      buttons: [
+        {
+          name: "Enroll Now",
+          link: "#aviation-101-form",
+          primary: false,
+        },
+        {
+          name: "Book a Discovery Flight",
+          link: "/discovery-flight",
+          primary: false,
+        },
+      ],
+    },
+
+    pageSummary: {
+      title: `What Is Aviation 101?`,
+      paragraphs: [
+        "In our Aviation 101 - Ground Training Program, students aged 15 to 18 are invited to embark on a 13-week journey. Every Saturday morning from 10 am to 12 pm, participants will delve into an immersive course, culminating in test preparation and qualification for the Private Pilot Written Exam.",
+        "Your children can be the part of the future generation of skilled pilots and aviators. The Youth Aviation Class at Sun City Aviation Academy provides a fun environment for learning how to fly.",
+      ],
+    },
+
+    expectAviation101: {
+      imagePath: "/src/assets/AeroCamp125-eyes.jpg",
+      imageAlt: "Sun City Aviation Academy youth program students",
+      heading: "Training Future Pilots",
+      upperHeading: "Aviation 101",
+      description: `The High School Aviation Program is a unique opportunity designed to introduce younger minds to the marvels of flight. Led by experienced instructors, this program immerses aspiring aviators in the world of aircraft, aerodynamics, and the science of flying. It's not just a course though; it's a pathway to ignite a lifelong passion for aviation from an early age.`,
+      paragraphs: [
+        "For parents, our commitment extends beyond the classroom. Sun City Aviation boasts a team of expert instructors dedicated to providing a safe and enriching learning environment. We believe in opening doors to diverse educational and career paths through aviation, and our curriculum prepares students to meet the requirements of the Airmen Certification Standards (ACS) laid out by the FAA. By the end of the course your child will be ready to take their FAA private pilot written exam.",
+        "Young aspiring aviators, aged 15 to 18, will find a dynamic and interactive learning environment. Our program lays the essential groundwork for future success in aviation, offering not just theoretical knowledge but practical experiences. With the opportunity to solo at 16 and achieve a private pilot certificate by 17, our program is designed not only to spark an interest in aviation but also to set participants on a trajectory toward success in the skies.",
+      ],
+      afterListParagraphs: [
+        "Don't miss the chance for your high schooler to enter into the exciting realm of aviation. Remember, our next Aviation 101 course kicks off soon, so register your student today!",
+        `For more information, feel free to call us at <a href="tel:7544232159" class="text-accent-500 font-semibold hover:underline">(754) 423-2159</a> or email <a href="mailto:info@suncityaviation.com" class="text-accent-500 font-semibold hover:underline">info@suncityaviation.com</a>. We look forward to welcoming your child to the Sun City Aviation family!`,
+        "The future generation of skilled pilots and aviators starts here! Enroll your child in the High School Aviation Program today!",
+      ],
+      finalButton: {
+        name: "Enroll Now",
+        link: "#aviation-101-form",
+        primary: true,
+      },
+    },
+
+    form: {
+      upperHeading: "Enroll Now",
+      heading: "Experience Aviation High School Program",
+      description: `Fill out the form below to enroll in the Experience Aviation High School Program at Sun City Aviation Academy. Our team will be in touch with you shortly to confirm your enrollment and provide you with more information about the program.`,
+      checkboxText: `I agree to <a href="/terms-of-service" target="_blank" class="font-semibold hover:underline">terms</a> & <a href="/privacy-policy" target="_blank" class="font-semibold hover:underline">conditions</a> provided by the company. By providing my phone number, I agree to receive text messages from the business.`,
+    },
+
+    bookCTA: {
+      upperHeading: "Still looking for more information on pilot training?",
+      headerH1: `Get Your <span class="text-accent-300">FREE</span><br> Pilot Training Guide!`,
+      paragraph:
+        "We have a free booklet which explains everything you need to know for your pilot certificate.",
+      buttons: [
+        {
+          name: "Download Now",
+          link: "/book-download",
+          primary: true,
+        },
+      ],
+      imagePath: "/src/assets/sun-city-book-mock-up.webp",
+      imageAlt: "Right Rudder Marketing team on the runway",
+    },
+
+    quizCTA: {
+      reverse: true,
+      upperHeading: "Take Our Quiz",
+      heading: "Do You Have What It Takes To Be A Pilot?",
+      descriptions: [
+        "Are you fascinated by the world of aviation? Have you dreamed of flying and exploring the skies? Do you often look upward at passing aircraft, wondering if you yourself could be in the cockpit?",
+        "Begin your journey in aviation with our brief quiz. Our quiz is designed to look at your aviation related knowledge and aptitude, alongside your unique personality traits and interests, to provide insights into whether a future in aviation aligns with your aspirations.",
+      ],
+      imagePath: "/src/assets/sun-city-aviation-academy-be-pilot-square.jpg",
+      imageAlt:
+        "sun city aviation academy student flying a plane over the city of miami",
+      buttons: [
+        {
+          name: "Take the Quiz",
+          link: "/quiz",
+          primary: true,
+        },
+      ],
+    },
+
+    flyWithUsCTA: {
+      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      headerH1: `<span class="text-primary-600">Start Your Aviation Journey</br></span>With a Discovery Flight`,
+      paragraph:
+        "Doing a discovery flight is the best way to determine if being a pilot is right for you. Whether you're trying it out for fun or want to fly professionally, doing a discovery flight will allow you to see the Ft. Lauderdale and Hollywood from a new perspective.",
+      buttons: [
+        {
+          name: "Schedule Today",
+          link: "/discovery-flight",
+          primary: true,
+        },
+      ],
+    },
+  },
+};
+export default aviation101Info;
