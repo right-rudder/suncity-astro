d6b2e1f9a2139ba3856f3544799a8f8a6f445a11
Raul Ospina <raminka13@hotmail.com>
2025-02-07 17:01:51 -0500

    Update: gTags

--- src/consts.ts
+++ src/consts.ts
@@ -4,14 +4,14 @@ export const SITE_TITLE =
 
 // CHANGE default meta description -> should only be used in homepage technically, but is there for a fallback in case a page is missed
 export const SITE_DESCRIPTION =
-  "Our mission is to train professional, safe, and proficient pilots, who will one day excel in the aviation industry. When you fly with the team at Sun City Aviation Academy, you're family. Individualized instruction means you're more than just a number. You're a future Sun City aviator.";
+  "Our mission is to train professional, safe, and proficient pilots, who will one day excel in the aviation industry. When you fly with the team at Sun City Aviation Academy, you're family. Individualized instruction means you're more than just a number. You're a future Sun City aviator. We offer a variety of flight training programs, from private pilot to commercial pilot, and everything in between. Our flight school is located in Pembroke Pines, Florida, and we serve the greater Miami area. Contact us today to learn more about our flight training programs.";
 
 //SEO tip -> keyword + city 1
 //Should have between 5-10 different localities to mix and match keywords
 //typical keywords = how to become a pilot, flight school near, private pilot, discovery flight, introductory flight, instrument rating, commercial certificate, certified flight instructor, airline programs, how to become an airline pilot
 // CHANGE default keywords -> should only be used in homepage technically, but is there for a fallback in case a page is missed
 export const KEYWORDS =
-  "Sun City Aviation Academy, located at KHWO - North Perry Airport in Hollywood, Florida, is your gateway to flight training excellence in the vibrant Miami-Fort Lauderdale metropolitan area. Our flight school serves aspiring pilots from Hollywood, Fort Lauderdale, Miami, and neighboring communities across Broward County. With our team of experienced instructors and a comprehensive range of aviation programs, we're committed to helping you achieve your aviation dreams right here in the heart of South Florida. Join us at Sun City Aviation Academy and embark on your journey to becoming a skilled pilot in one of the nation's most exciting aviation hubs.";
+  "flight school near me, KHWO, KHWO airport, how to become a pilot, private pilot, discovery flight, introductory flight, instrument rating, commercial certificate, certified flight instructor, airline programs, how to become an airline pilot, Miami, South Florida, Fort Lauderdale, Hollywood, Pembroke Pines, Miramar, Weston, Davie, Plantation, Cooper City, Dania Beach, Hallandale Beach, Aventura, North Miami Beach, Miami Beach, Miami Gardens, Hialeah, Opa-locka, Miami Lakes, Miami Springs, Miami Shores, Miami Gardens, Miami Dade, Broward County";
 
 export const OG_IMAGE = "/SunCity-OG-image.png";
 
