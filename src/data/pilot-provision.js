6a42ff7092641adc4946b2d1d3586e922de71701
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-06-12 18:04:37 -0300

    Add empty data

---
+++ src/data/pilot-provision.js
@@ -0,0 +1,96 @@
+const pilotProvision = {
+  data: {
+    // SEO Header Stuff
+    pageTitle: "",
+    pageDescription: "",
+    pageKeywords: "",
+
+    // Top header
+    header: {
+      stars: "",
+      imagePath: "",
+      imageAlt: "",
+      headerH1: "",
+      paragraph: "",
+      buttons: [
+        {
+          name: "",
+          link: "",
+          primary: "",
+        },
+        {
+          name: "",
+          link: "",
+          primary: "",
+        },
+      ],
+    },
+
+    pageSummary: {
+      title: "",
+      paragraphs: ["", "", ""],
+    },
+
+    privateRoadmap: {
+      upperHeading: "",
+      heading: "",
+      descriptions: [""],
+      steps: [
+        {
+          title: "",
+          paragraphs: ["", "", "", ""],
+          imagePath: "",
+          imageAlt: "",
+        },
+        {
+          title: "",
+          paragraphs: ["", "", "", "", ""],
+          imagePath: "",
+          imageAlt: "",
+        },
+        {
+          title: "",
+          paragraphs: ["", "", "", ""],
+          imagePath: "",
+          imageAlt: "",
+        },
+      ],
+      finalParagraph: "",
+      finalButton: {
+        name: "",
+        link: "",
+        primary: "",
+      },
+    },
+
+    bookCTA: {
+      upperHeading: "",
+      headerH1: "",
+      paragraph: "",
+      buttons: [
+        {
+          name: "",
+          link: "",
+          primary: "",
+        },
+      ],
+      imagePath: "",
+      imageAlt: "",
+    },
+
+    flyWithUsCTA: {
+      imagePath: "",
+      imageAlt: "",
+      headerH1: "",
+      paragraph: "",
+      buttons: [
+        {
+          name: "",
+          link: "",
+          primary: "",
+        },
+      ],
+    },
+  },
+};
+export default pilotProvision;
