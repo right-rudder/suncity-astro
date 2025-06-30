87e7c834805b310abe7c66664ee095335363d4c3
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-06-30 12:13:04 -0300

    Update provision store images

--- src/data/pilotProvisionSwiper.js
+++ src/data/pilotProvisionSwiper.js
@@ -6,7 +6,7 @@ const pilotProvisionSwiper = {
   cards: [
     {
       title: "Books",
-      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imagePath: "/src/assets/suncity_pilot_provisions_store_2.jpg",
       imageAlt: "Aviation-themed books stacked on a table",
       description:
         "Explore aviation history, flight manuals, and children’s books that inspire a love for the skies.",
@@ -14,7 +14,7 @@ const pilotProvisionSwiper = {
     },
     {
       title: "Gifts & Collectibles",
-      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imagePath: "/src/assets/suncity_pilot_provisions_store_4.jpg",
       imageAlt: "Model airplane, aviation mugs, and themed souvenirs",
       description:
         "Perfect for aviation lovers: mugs, model planes, magnets, keychains, and limited-edition memorabilia.",
@@ -22,7 +22,7 @@ const pilotProvisionSwiper = {
     },
     {
       title: "Wearables",
-      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imagePath: "/src/assets/suncity_pilot_provisions_store_1.jpg",
       imageAlt: "Sun City Aviation branded shirts and caps on display",
       description:
         "Sun City–branded hats, t-shirts, sweaters, and more—comfortable gear for fans and students alike.",
@@ -30,7 +30,7 @@ const pilotProvisionSwiper = {
     },
     {
       title: "Office & Lifestyle",
-      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imagePath: "/src/assets/suncity_pilot_provisions_store_3.jpg",
       imageAlt: "Aviation-themed pens, notebooks, and desk decor",
       description:
         "Branded notebooks, pens, desk organizers, water bottles, and essentials for home or office.",
